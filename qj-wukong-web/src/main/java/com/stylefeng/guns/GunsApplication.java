29dc9870054c0a74f84b0ff7bb4fda4561aea779
zhoushunjia <947723090@qq.com>
2018-06-09 22:21:04 +0800

    test

--- qj-wukong-web/src/main/java/com/stylefeng/guns/GunsApplication.java
+++ qj-wukong-web/src/main/java/com/stylefeng/guns/GunsApplication.java
@@ -17,6 +17,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * test666
  * tets666
  * 666
+ * testtij
  */
 @SpringBootApplication
 public class GunsApplication extends WebMvcConfigurerAdapter {
